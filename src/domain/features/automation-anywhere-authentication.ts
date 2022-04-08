19dbc7f8287bd5e8297b278c4e6b6577e958a3fa
Stephano Balbinot <stephanobalbinot@hotmail.com>
2022-04-07 22:32:29 -0300

    feat: add automation anywhere authentication feature

---
+++ src/domain/features/automation-anywhere-authentication.ts
@@ -0,0 +1,14 @@
+import { AccessToken } from '@/domain/models'
+import { AuthenticationError } from '@/domain/errors'
+
+export interface AutomationAnywhereAuthentication {
+  perform: (params: AutomationAnywhereAuthentication.Params) => Promise<AutomationAnywhereAuthentication.Result>
+}
+
+namespace AutomationAnywhereAuthentication {
+  export type Params = {
+    token: string
+  }
+
+  export type Result = AccessToken | AuthenticationError
+}
