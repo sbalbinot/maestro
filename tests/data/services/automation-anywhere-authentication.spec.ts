1f99148071d6a2c8a74f19f0c5db2aa2850d9ad6
Stephano Balbinot <stephanobalbinot@hotmail.com>
2022-04-10 19:22:41 -0300

    refactor: change test strategy to use jest mocks

--- tests/data/services/automation-anywhere-authentication.spec.ts
+++ tests/data/services/automation-anywhere-authentication.spec.ts
@@ -1,33 +1,24 @@
 import { AuthenticationError } from '@/domain/errors'
 import { AutomationAnywhereAuthenticationService } from '@/data/services'
-import { LoadAutomationAnywhereUserApi } from '@/data/contracts/apis'
-
-class LoadAutomationAnywhereUserApiSpy implements LoadAutomationAnywhereUserApi {
-  token?: string
-  callsCount = 0
-  result = undefined
-
-  async load (params: LoadAutomationAnywhereUserApi.Params): Promise<LoadAutomationAnywhereUserApi.Result> {
-    this.token = params.token
-    this.callsCount++
-    return this.result
-  }
-}
 
 describe('AutomationAnywhereAuthenticationService', () => {
   it('should call LoadAutomationAnywhereUserApi with correct params', async () => {
-    const loadAutomationAnywhereUserApi = new LoadAutomationAnywhereUserApiSpy()
+    const loadAutomationAnywhereUserApi = {
+      load: jest.fn()
+    }
     const sut = new AutomationAnywhereAuthenticationService(loadAutomationAnywhereUserApi)
 
     await sut.perform({ token: 'any_token' })
 
-    expect(loadAutomationAnywhereUserApi.token).toBe('any_token')
-    expect(loadAutomationAnywhereUserApi.callsCount).toBe(1)
+    expect(loadAutomationAnywhereUserApi.load).toHaveBeenCalledWith({ token: 'any_token' })
+    expect(loadAutomationAnywhereUserApi.load).toHaveBeenCalledTimes(1)
   })
 
   it('should return AuthenticationError when LoadAutomationAnywhereUserApi returns undefined', async () => {
-    const loadAutomationAnywhereUserApi = new LoadAutomationAnywhereUserApiSpy()
-    loadAutomationAnywhereUserApi.result = undefined
+    const loadAutomationAnywhereUserApi = {
+      load: jest.fn()
+    }
+    loadAutomationAnywhereUserApi.load.mockResolvedValueOnce(undefined)
     const sut = new AutomationAnywhereAuthenticationService(loadAutomationAnywhereUserApi)
 
     const authResult = await sut.perform({ token: 'any_token' })
