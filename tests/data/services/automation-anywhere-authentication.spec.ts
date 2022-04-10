25cc45e34af0621cd1f34e7da35472d58d1bf2a9
Stephano Balbinot <stephanobalbinot@hotmail.com>
2022-04-10 19:30:32 -0300

    refactor: simplify code

--- tests/data/services/automation-anywhere-authentication.spec.ts
+++ tests/data/services/automation-anywhere-authentication.spec.ts
@@ -7,6 +7,7 @@ import { mock, MockProxy } from 'jest-mock-extended'
 describe('AutomationAnywhereAuthenticationService', () => {
   let loadAutomationAnywhereUserApi: MockProxy<LoadAutomationAnywhereUserApi>
   let sut: AutomationAnywhereAuthenticationService
+  const token = 'any_token'
 
   beforeEach(() => {
     loadAutomationAnywhereUserApi = mock()
@@ -14,16 +15,16 @@ describe('AutomationAnywhereAuthenticationService', () => {
   })
 
   it('should call LoadAutomationAnywhereUserApi with correct params', async () => {
-    await sut.perform({ token: 'any_token' })
+    await sut.perform({ token })
 
-    expect(loadAutomationAnywhereUserApi.load).toHaveBeenCalledWith({ token: 'any_token' })
+    expect(loadAutomationAnywhereUserApi.load).toHaveBeenCalledWith({ token })
     expect(loadAutomationAnywhereUserApi.load).toHaveBeenCalledTimes(1)
   })
 
   it('should return AuthenticationError when LoadAutomationAnywhereUserApi returns undefined', async () => {
     loadAutomationAnywhereUserApi.load.mockResolvedValueOnce(undefined)
 
-    const authResult = await sut.perform({ token: 'any_token' })
+    const authResult = await sut.perform({ token })
 
     expect(authResult).toEqual(new AuthenticationError())
   })
