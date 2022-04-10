cdb6ee0466c16314748d7f83e8697cf2ff253cea
Stephano Balbinot <stephanobalbinot@hotmail.com>
2022-04-10 19:18:50 -0300

    test: ensure AuthenticationService calls LoadAutomationAnywhereUserApi once

--- tests/data/services/automation-anywhere-authentication.spec.ts
+++ tests/data/services/automation-anywhere-authentication.spec.ts
@@ -4,10 +4,12 @@ import { LoadAutomationAnywhereUserApi } from '@/data/contracts/apis'
 
 class LoadAutomationAnywhereUserApiSpy implements LoadAutomationAnywhereUserApi {
   token?: string
+  callsCount = 0
   result = undefined
 
   async load (params: LoadAutomationAnywhereUserApi.Params): Promise<LoadAutomationAnywhereUserApi.Result> {
     this.token = params.token
+    this.callsCount++
     return this.result
   }
 }
@@ -20,6 +22,7 @@ describe('AutomationAnywhereAuthenticationService', () => {
     await sut.perform({ token: 'any_token' })
 
     expect(loadAutomationAnywhereUserApi.token).toBe('any_token')
+    expect(loadAutomationAnywhereUserApi.callsCount).toBe(1)
   })
 
   it('should return AuthenticationError when LoadAutomationAnywhereUserApi returns undefined', async () => {
