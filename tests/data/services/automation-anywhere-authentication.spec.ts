d839846c6b117090fb6753cdc3bc88eeec9c5ded
Stephano Balbinot <stephanobalbinot@hotmail.com>
2022-04-10 19:27:31 -0300

    refactor: move SUT creation to a factory method

--- tests/data/services/automation-anywhere-authentication.spec.ts
+++ tests/data/services/automation-anywhere-authentication.spec.ts
@@ -2,12 +2,25 @@ import { AuthenticationError } from '@/domain/errors'
 import { AutomationAnywhereAuthenticationService } from '@/data/services'
 import { LoadAutomationAnywhereUserApi } from '@/data/contracts/apis'
 
-import { mock } from 'jest-mock-extended'
+import { mock, MockProxy } from 'jest-mock-extended'
+
+type SutTypes = {
+  sut: AutomationAnywhereAuthenticationService
+  loadAutomationAnywhereUserApi: MockProxy<LoadAutomationAnywhereUserApi>
+}
+
+const makeSut = (): SutTypes => {
+  const loadAutomationAnywhereUserApi = mock<LoadAutomationAnywhereUserApi>()
+  const sut = new AutomationAnywhereAuthenticationService(loadAutomationAnywhereUserApi)
+  return {
+    sut,
+    loadAutomationAnywhereUserApi
+  }
+}
 
 describe('AutomationAnywhereAuthenticationService', () => {
   it('should call LoadAutomationAnywhereUserApi with correct params', async () => {
-    const loadAutomationAnywhereUserApi = mock<LoadAutomationAnywhereUserApi>()
-    const sut = new AutomationAnywhereAuthenticationService(loadAutomationAnywhereUserApi)
+    const { sut, loadAutomationAnywhereUserApi } = makeSut()
 
     await sut.perform({ token: 'any_token' })
 
@@ -16,9 +29,8 @@ describe('AutomationAnywhereAuthenticationService', () => {
   })
 
   it('should return AuthenticationError when LoadAutomationAnywhereUserApi returns undefined', async () => {
-    const loadAutomationAnywhereUserApi = mock<LoadAutomationAnywhereUserApi>()
+    const { sut, loadAutomationAnywhereUserApi } = makeSut()
     loadAutomationAnywhereUserApi.load.mockResolvedValueOnce(undefined)
-    const sut = new AutomationAnywhereAuthenticationService(loadAutomationAnywhereUserApi)
 
     const authResult = await sut.perform({ token: 'any_token' })
 
